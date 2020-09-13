b28a039b3eeaa580ea7903d3933cb7f0e16575bf
Maksim Stefanenko <maksim.stefanenko@welmed.it>
2020-09-13 19:14:50 +0300

    Remove unused packages

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  modulePaths: ['<rootDir>/src/'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-};
