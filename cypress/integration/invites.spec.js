2f40a4419abcc4ed0a61c542e05ec6856fdd0d8b
Maksim Stefanenko <maksim.stefanenko@welmed.it>
2020-09-15 12:05:02 +0300

    Remove sypress

--- cypress/integration/invites.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Invites Page', () => {
-  it('successfully loads', () => {
-    cy.visit('http://localhost:8080');
-    cy.login();
-    cy.wait(3000);
-    cy.contains('Invites').click();
-    cy.location().should(location => expect(location.pathname).to.match(/inviteList/));
-  });
-});
